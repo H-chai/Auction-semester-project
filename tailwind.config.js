3a9d78e9aee25d7b66d2af5d6f606e9945b2cbf2
H-chai <143179912+H-chai@users.noreply.github.com>
2024-11-15 11:52:59 +0100

    add custom fontFamily and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./**/*.{html,js}", "!./node_modules/**/*"],
+  content: ['./**/*.{html,js}', '!./node_modules/**/*'],
   theme: {
+    fontFamily: {
+      display: ['General Sans', 'sans-serif'],
+      body: ['Gambetta', 'serif'],
+    },
+    colors: {
+      black: '#212121',
+      blue: '#034C8C',
+      orange: '#FFA726',
+      gray: '#727272',
+      'light-gray': '#9E9E9E',
+      green: '#8BC34A',
+      red: '#D32F2F',
+      'off-white': '#F5F5F5',
+      outline: '#B0BEC5',
+      'outline-light': '#E0E0E0',
+    },
     extend: {},
   },
   plugins: [],
