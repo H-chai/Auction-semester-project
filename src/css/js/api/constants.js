69c9f297f1efbf7b15a3ec086012252891ae3655
H-chai <143179912+H-chai@users.noreply.github.com>
2024-11-19 11:53:20 +0100

    Add API constants for base URL and endpoints

---
+++ src/css/js/api/constants.js
@@ -0,0 +1,7 @@
+export const API_KEY = '42d1daed-d7a5-4f39-b3da-5f11248409c0';
+
+export const API_BASE = 'https://v2.api.noroff.dev';
+
+export const API_AUTH = `${API_BASE}/auth`;
+
+export const API_AUCTION = `${API_BASE}/auction`;
