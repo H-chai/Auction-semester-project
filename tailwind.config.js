11134ed5d68554b829508970438660dd06086fb8
H-chai <143179912+H-chai@users.noreply.github.com>
2024-11-22 10:21:48 +0100

    Add custom animation for image slider

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,6 +28,35 @@ export default {
       borderRadius: {
         md: '0.25rem',
       },
+      animation: {
+        'slider-01': 'slider-01 32s infinite',
+        'slider-02': 'slider-02 32s infinite',
+        'slider-03': 'slider-03 32s infinite',
+      },
+      keyframes: {
+        'slider-01': {
+          '0%': { opacity: 1 },
+          '30%': { opacity: 1 },
+          '40%': { opacity: 0 },
+          '90%': { opacity: 0 },
+          '100%': { opacity: 1 },
+        },
+        'slider-02': {
+          '0%': { opacity: 0 },
+          '30%': { opacity: 0 },
+          '40%': { opacity: 1 },
+          '60%': { opacity: 1 },
+          '70%': { opacity: 0 },
+          '100%': { opacity: 0 },
+        },
+        'slider-03': {
+          '0%': { opacity: 0 },
+          '60%': { opacity: 0 },
+          '70%': { opacity: 1 },
+          '90%': { opacity: 1 },
+          '100%': { opacity: 0 },
+        },
+      },
     },
   },
   plugins: [],
