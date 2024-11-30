4a5f70af484a1cee8650a898f1fbb2c7aa9cf8b3
H-chai <143179912+H-chai@users.noreply.github.com>
2024-11-30 11:45:28 +0100

    Remove console.log

--- src/js/ui/components/utils/getLatestImages.js
+++ src/js/ui/components/utils/getLatestImages.js
@@ -5,14 +5,10 @@ const listings = await api.listing.get24Listings();
 
 export function getLatestImages() {
   const { data } = listings;
-  //console.log(data);
   const mediaArray = data.map((listing) => listing.media[0]);
-  //console.log(mediaArray);
   const filterMediaArray = mediaArray.filter((media) => media !== undefined);
-  //console.log(filterMediaArray);
   const urlArray = filterMediaArray.map((media) => media.url);
   const latestImageArray = urlArray.slice(0, 3);
-  //console.log(latestImageArray);
 
   return latestImageArray;
 }
