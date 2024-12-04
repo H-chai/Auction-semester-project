066256a471a6d2dd7ceb5f57dd978a60c5913221
H-chai <143179912+H-chai@users.noreply.github.com>
2024-12-04 13:37:44 +0100

    update with MPA (Multi-Page Application)

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,30 @@
+import { dirname, resolve } from 'path';
 import { defineConfig } from 'vite';
+import { fileURLToPath } from 'url';
 import tailwindcss from 'tailwindcss';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export default defineConfig({
+  appType: 'mpa',
   plugins: [tailwindcss()],
   css: {
     postcss: './postcss.config.js',
+  },
+  build: {
+    target: 'esnext',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, './index.html'),
+        login: resolve(__dirname, './auth/login/index.html'),
+        register: resolve(__dirname, './auth/register/index.html'),
+        listing: resolve(__dirname, './listing/index.html'),
+        listingCreate: resolve(__dirname, './listing/create/index.html'),
+        listingUpdate: resolve(__dirname, './listing/update/index.html'),
+        profile: resolve(__dirname, './profile/index.html'),
+        profileUpdate: resolve(__dirname, './profile/update/index.html'),
+      },
+    },
   },
 });
